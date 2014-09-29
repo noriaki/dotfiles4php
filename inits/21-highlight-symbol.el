06dc08e9bffa038a80fb1c55473d2aa29b29d9f2
Noriaki UCHIYAMA <uchiyama.noriaki@gmail.com>
2014-09-29 22:09:42 +0000

    first commit

---
+++ inits/21-highlight-symbol.el
@@ -0,0 +1,4 @@
+(require 'highlight-symbol)
+
+;; 使いたい色を設定、repeatしてくれる
+(setq highlight-symbol-colors '("DarkOrange" "DodgerBlue1" "DeepPink1"))
---
+++ inits/40-sequential-command.el
@@ -0,0 +1,13 @@
+(defvar sequential-command-config-version "$Id: sequential-command-config.el,v 1.3 2009/03/22 09:09:58 rubikitch Exp $")
+(require 'sequential-command)
+
+(define-sequential-command seq-home
+  beginning-of-line beginning-of-buffer seq-return)
+(define-sequential-command seq-end
+  end-of-line end-of-buffer seq-return)
+
+(global-set-key "\C-a" 'seq-home)
+(global-set-key "\C-e" 'seq-end)
+(when (require 'org nil t)
+  (define-key org-mode-map "\C-a" 'org-seq-home)
+  (define-key org-mode-map "\C-e" 'org-seq-end))
