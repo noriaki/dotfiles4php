aea923f6e9c31c9b33f6fec0ed466b3eac79131f
Noriaki UCHIYAMA <uchiyama.noriaki@gmail.com>
2014-10-13 23:43:00 +0900

    add haml auto compile

--- inits/30-haml.el
+++ inits/30-haml.el
@@ -1,1 +1,14 @@
 (require 'haml-mode)
+;; flymake haml
+(add-hook 'haml-mode-hook 'flymake-haml-load)
+;; hamlからhtmlへコンパイル
+(defun haml-compile ()
+  (interactive)
+  (let ((from buffer-file-name)
+        (to (replace-regexp-in-string "\.haml$" ".html" buffer-file-name)))
+    (shell-command-to-string
+     (concat "haml" " --double-quote-attributes " from " " to))))
+;; 保存時に自動でhamlからhtmlへコンパイル
+(add-hook 'haml-mode-hook
+          '(lambda ()
+             (add-hook 'after-save-hook 'haml-compile)))
